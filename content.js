726d864a401b41e7f495813cbc025afd75af0f62
AsgerLanghoff <54887248+AsgerLanghoff@users.noreply.github.com>
2020-04-14 15:51:59 +0200

    forsøg på at injecte information
    forsøg på at indsætte information i momondos html kode ved hjælp af jquery.

--- content.js
+++ content.js
@@ -7,7 +7,7 @@
 //  let x = document.getElementsByClassName("planeDetails details-subheading")[i];
 //console.log(x.innerText);
 //}
-for (let i =0 ; i<document.getElementsByClassName("resultWrapper").length; i++) {
+/*for (let i =0 ; i<document.getElementsByClassName("resultWrapper").length; i++) {
     let x = document.getElementsByClassName("resultWrapper")[i];
     for (let j =0 ; j<x.getElementsByClassName("planeDetails details-subheading").length; j++) {
         let y = x.getElementsByClassName("planeDetails details-subheading")[j];
@@ -18,12 +18,33 @@ for (let i =0 ; i<document.getElementsByClassName("resultWrapper").length; i++) 
 
     console.log("next plane");
 }
+*/
 
 
+//function Test() {
+   /* var image = document.createElement("img");
+    image.src = chrome.runtime.getURL("icon.png");
+    var placeCanvas = document.getElementsByClassName("planeDetails details-subheading");
+    var place = placeCanvas.getBoundingClientRect();
+    var x = place.left;
+    var y = place.top;
+    document.body.appendChild(image);
+    //image.style.width = document.getElementsByClassName("Flights-Results-FlightPriceSection right-alignment sleek").Width/2;
+    //image.style.height =document.getElementsByClassName("Flights-Results-FlightPriceSection right-alignment sleek").Height/2;
+    //image.style.width = document.Width/2;
+    //image.style.height = document.Height/2;
+    //alert("tester tester");
+    console.log(x);
+}*/
+   function test() {
 
-
-
-
-
-
-// }
+       var btnParent = $(".resultwrapper");
+       console.log(btnParent);
+       var buttons = btnParent.find(getElementsByClassName("buttom"));
+       console.log(buttons);
+       for (var i = 0; i < buttons.length; i++) {
+           $(buttons[i]).html("<b>Hey atle"+i+"</b>");
+           console.log(buttons[i]);
+       }
+   }
+   test();
