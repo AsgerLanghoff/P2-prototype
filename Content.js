eed204442196ab02b5d316d8e2c80126badbfce3
AsgerLanghoff <54887248+AsgerLanghoff@users.noreply.github.com>
2020-04-22 14:59:14 +0200

    .

--- Content.js
+++ Content.js
@@ -21,7 +21,7 @@
 /*$(".top-row").html('<div class="billede"> <div id="picture"> </div> <img src="chrome-extension://dhdbohbajnnloobkandkolhoonjcbdig/logo.png" alt="Test billede" align="left"> </div> ' +
     '<div id="popup" style="display: none">billede </div>');
 
- */
+*/
 
 
 function letsTryThis2() {
