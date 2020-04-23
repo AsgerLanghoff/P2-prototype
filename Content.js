709d1c16ba6325f8928d01b2d7b5222e0402197a
AsgerLanghoff <54887248+AsgerLanghoff@users.noreply.github.com>
2020-04-23 09:58:21 +0200

    .

--- Content.js
+++ Content.js
@@ -3,19 +3,6 @@
 // while(2>1) {
 
 
-/*for (let i =0 ; i<document.getElementsByClassName("resultWrapper").length; i++) {
-    let x = document.getElementsByClassName("resultWrapper")[i];
-    for (let j =0 ; j<x.getElementsByClassName("planeDetails details-subheading").length; j++) {
-        let y = x.getElementsByClassName("planeDetails details-subheading")[j];
-        let z = x.getElementsByClassName("segmentDuration text-row")[j];
-        console.log(y.innerText);
-        console.log(z.innerText);
-    }
-
-    console.log("next plane");
-}
-*/
-
 //$.when(load).done(function() {
 // sidste kørte function fra momondo er under id: "ampim"
 /*$(".top-row").html('<div class="billede"> <div id="picture"> </div> <img src="chrome-extension://dhdbohbajnnloobkandkolhoonjcbdig/logo.png" alt="Test billede" align="left"> </div> ' +
