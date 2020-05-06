5f45e9bc9b4915b98e8f889b529d5750db2f04e8
AtleNyhus <55181788+AtleNyhus@users.noreply.github.com>
2020-05-06 11:17:52 +0200

    flere fly

--- Database.js
+++ Database.js
@@ -11,20 +11,23 @@ class Database {
         /*Airbus A321 */ [1909, 3925, 5270, 7157, 8970, 12456, 15818, 19094, 22308],
         /*Boeing 757-200*/ [2159, 4435, 5939, 8054, 10085, 13984, 17740, 21398, 24983, 28159],
         /*Airbus A319*/[1596, 3259, 4323, 5830, 7271, 10026, 12668, 15233, 17741, 20203],
-        /*Boeing 737-900ER*/ [1782, 3641, 4839, 6533, 8154, 11255, 14233, 17125, 19954, 22733, 25471]
-        /*Embraer E175 Vi skal snakke om hvad der skal ske hvis flyet ikke er på ICAO listen*/
+        /*Boeing 737-900ER*/ [1782, 3641, 4839, 6533, 8154, 11255, 14233, 17125, 19954, 22733, 25471],
+        /*Embraer E175*/ [1113, 2240, 2989, 3953, 4890, 6725], //Embraer har koden E7W, som svarer til E75 i ICAOs Equivalent Aircraft Mapping
+        /*Bombardier CRJ900*/ [1119, 2251, 2994, 3949, 4872, 6664],
+        /*Bombardier CRJ700*/ [1043, 2092, 2772, 3642, 4475, 6076],
+
     ];
 
     FUEL_CONSUMPTION_OTHER = [1672, 3430, 4585, 6212, 7772, 10766, 13648, 16452, 45056, 51831, 58678, 65621, 72666, 79159, 85687, 89019, 91982, 94586, 96840, 98752]; // In reality, these number can vary +- 20'000...
 
-    NUMBER_OF_Y_SEATS = [162, 141, 96, 176, 200, 160, 177]; //The position in this array corresponds to LIST_OF_PLANES. Y_Seats = economy seats
+    NUMBER_OF_Y_SEATS = [162, 141, 96, 176, 200, 160, 177, 90, 68]; //The position in this array corresponds to LIST_OF_PLANES. Y_Seats = economy seats
     NUMBER_OF_Y_SEATS_OTHER = 165 ; //This number is open for discussion
 
     PAX_LOAD_FACTOR = 80; //This number is open for discussion
 
     PAX_TO_FREIGHT_FACTOR = 96; //This number is open for discussion
 
-    KM_PER_HOUR = [842, 828, 840, 833, 850, 828, 925]; ////The position in this array corresponds to LIST_OF_PLANES. Enter the cruise speed of the aircraft
+    KM_PER_HOUR = [842, 828, 840, 833, 850, 828, 925, 830, 829 ]; ////The position in this array corresponds to LIST_OF_PLANES. Enter the cruise speed of the aircraft
     KM_PER_HOUR_OTHER = 850;
 
 
