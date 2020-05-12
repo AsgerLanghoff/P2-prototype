e0b7570f3112cfe6e0ba019d369e88c4c309ba0d
AsgerLanghoff <54887248+AsgerLanghoff@users.noreply.github.com>
2020-05-12 10:33:54 +0200

    s

--- Database.js
+++ Database.js
@@ -5,7 +5,7 @@ class Database {
     LIST_OF_DISTANCE = [125, 250, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500]; //Nautical Miles. This distance is in nautical miles.
 
     FUEL_CONSUMPTION = [
-       /*Boeing 737-800*/ [1715, 3494, 4621, 6221, 7749, 10666, 13460, 16170, 18818, 21415, 23972], //This line corresponds to LIST_OF_PLANES[0], aka "Boeing 737-800". The numbers are pulled from https://www.icao.int/environmental-protection/CarbonOffset/Documents/Methodology%20ICAO%20Carbon%20Calculator_v10-2017.pdf
+        /*Boeing 737-800*/ [1715, 3494, 4621, 6221, 7749, 10666, 13460, 16170, 18818, 21415, 23972], //This line corresponds to LIST_OF_PLANES[0], aka "Boeing 737-800". The numbers are pulled from https://www.icao.int/environmental-protection/CarbonOffset/Documents/Methodology%20ICAO%20Carbon%20Calculator_v10-2017.pdf
         /*Boeing 737-700*/ [1586, 3202, 4173, 5570, 6895, 9410, 11804, 14113, 16360, 18558, 20715],
         /*Airbus A320*/ [1672, 3430, 4585, 6212, 7772, 10766, 13648, 16452],
         /*Airbus A321 */ [1909, 3925, 5270, 7157, 8970, 12456, 15818, 19094, 22308],
@@ -21,13 +21,13 @@ class Database {
     FUEL_CONSUMPTION_OTHER = [1672, 3430, 4585, 6212, 7772, 10766, 13648, 16452, 45056, 51831, 58678, 65621, 72666, 79159, 85687, 89019, 91982, 94586, 96840, 98752]; // In reality, these number can vary +- 20'000...
 
     NUMBER_OF_Y_SEATS = [162, 141, 96, 176, 200, 160, 177, 90, 68]; //The position in this array corresponds to LIST_OF_PLANES. Y_Seats = economy seats
-    NUMBER_OF_Y_SEATS_OTHER = 165 ; //This number is open for discussion
+    NUMBER_OF_Y_SEATS_OTHER = 165; //This number is open for discussion
 
     PAX_LOAD_FACTOR = 80; //This number is open for discussion
 
     PAX_TO_FREIGHT_FACTOR = 96; //This number is open for discussion
 
-    KM_PER_HOUR = [842, 828, 840, 833, 850, 828, 925, 830, 829 ]; ////The position in this array corresponds to LIST_OF_PLANES. Enter the cruise speed of the aircraft
+    KM_PER_HOUR = [842, 828, 840, 833, 850, 828, 925, 830, 829]; ////The position in this array corresponds to LIST_OF_PLANES. Enter the cruise speed of the aircraft
     KM_PER_HOUR_OTHER = 850;
 
 
@@ -50,12 +50,12 @@ class Database {
         for (let i = 0; i < this.LIST_OF_DISTANCE.length; i++) {
             let KM = this.getKMPerHour(a);
             let dist = this.getListOfDistanceAsKm(i);
-            currentNum = (flightTime/60)*KM - dist;
+            currentNum = (flightTime / 60) * KM - dist;
             currentNum = Math.abs(currentNum);
             if (currentNum < closestNum || i == 0) {
                 closestNum = currentNum;
-                closestIndex = i -1;//-1 for adjusting the number. THIS MIGHT CHANGE
-                console.log("index"+closestIndex);
+                closestIndex = i - 1;//-1 for adjusting the number. THIS MIGHT CHANGE
+                console.log("index" + closestIndex);
             }
         }
 
@@ -73,11 +73,11 @@ class Database {
         for (let i = 0; i < this.LIST_OF_DISTANCE.length; i++) {
             let KM = this.getKMPerHourOther();
             let dist = this.getListOfDistanceAsKm(i);
-            currentNum = (flightTime/60)*KM - dist;
+            currentNum = (flightTime / 60) * KM - dist;
             currentNum = Math.abs(currentNum);
             if (currentNum < closestNum || i == 0) {
                 closestNum = currentNum;
-                closestIndex = i -1; //-1 for adjusting the number. THIS MIGHT CHANGE
+                closestIndex = i - 1; //-1 for adjusting the number. THIS MIGHT CHANGE
             }
         }
         return this.FUEL_CONSUMPTION_OTHER[closestIndex];
@@ -110,7 +110,7 @@ class Database {
         if (a != -1) {
             return this.KM_PER_HOUR[a];
         } else {
-           return this.getKMPerHourOther()
+            return this.getKMPerHourOther()
         }
     }
 
