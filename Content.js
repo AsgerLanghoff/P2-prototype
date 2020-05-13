fcb34662a04681897d3a9d98d78a18e4e3167ac6
AsgerLanghoff <54887248+AsgerLanghoff@users.noreply.github.com>
2020-05-13 16:52:25 +0200

    .

--- Content.js
+++ Content.js
@@ -26,20 +26,6 @@ function showMinMax(something, string) {
 }
 
 
-function bubbleSort(arr){
-    var len = arr.length;
-    for (var i = len-1; i>=0; i--){
-        for(var j = 1; j<=i; j++){
-            if(arr[j-1]>arr[j]){
-                var temp = arr[j-1];
-                arr[j-1] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-    return arr;
-}
-
 
 $(function () {
     setTimeout(function () {
